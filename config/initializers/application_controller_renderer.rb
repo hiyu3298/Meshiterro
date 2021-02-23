c6316da6bd7478e160fad98a49539557c9b8ce40
hiyu3298 <akamatsu11218991@gmail.com>
2021-02-23 09:19:40 +0000

    [Add] 画像投稿フォームの作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '0f231e841259fee78e8c31e4e930c56e9c538116ad793d52bc67b4c02796b0b706ecefcdd742a00f360e695fa10d02fa130c867000c82ece1f1e000ffd909d02'
